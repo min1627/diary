3fc1bc8727f9dacda9bc542b73f338bdb43435ca
min1627 <minseok@minseokui-MacBook-Pro.local>
2015-10-21 17:02:56 +0900

    heroku 페북아디 세팅

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1666980640186241', 'f5011a9a8c1d709fb492144477116910'
+  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
 end
