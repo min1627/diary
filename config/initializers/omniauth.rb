807facbb1c2a9a0b4533ed0b99754ce01593ba9f
min1627 <minseok@minseokui-MacBook-Pro.local>
2015-10-21 17:59:26 +0900

    omniauth 세팅 수정

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+    provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], {:scope => 'email, public_profile, user_friends', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
   else
     provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
   end
