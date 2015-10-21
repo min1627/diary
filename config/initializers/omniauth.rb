698949c40035d06ccbd1f8ec1a69429522c56a1f
min1627 <minseok@minseokui-MacBook-Pro.local>
2015-10-21 17:40:20 +0900

    stage 별 FB Key 구분

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,9 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
-  client_options: { ssl: { 
-        ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
-        ca_path: "/etc/ssl/certs"
-    }}
-  }
+  if Rails.env.production?
+    provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
+  else
+    provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
+  end
 end
